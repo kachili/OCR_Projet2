1b30a262b29ffe1f3c8b2b63049c1950790f1f84
Khelifa ACHILI <k.achili@free.fr>
2021-12-27 19:56:04 +0100

    résolution de bugs dans 2 programmes

--- unecategorietousleslivres.py
+++ unecategorietousleslivres.py
@@ -6,13 +6,11 @@ import csv
 import requests
 from bs4 import BeautifulSoup
 from math import *
-# on initialise url à l'adresse url de la page qu'on veut extraire: une categorie
-#url = 'https://books.toscrape.com/catalogue/category/books/travel_2/index.html' # Categorie Travel 1 page
-#url = 'https://books.toscrape.com/catalogue/category/books/mystery_3/index.html'# Categrie Mystery 2 pages
-url = 'https://books.toscrape.com/catalogue/category/books/sequential-art_5/index.html' # 4 pages
+from unecategorieunlivre import extract_book
+import pprint
 
-# ..... A DEFINIR APRES fonction qui permet d'extraire toutes les données d'un livre
-# def extract_book(url_book):
+# on initialise url à l'adresse url de la page qu'on veut extraire: une categorie
+url = 'https://books.toscrape.com/catalogue/category/books/mystery_3/index.html' #OK
 
 # méthode .get()pour récupérer les données HTML dans la variable reponse
 reponse = requests.get(url)
@@ -33,7 +31,7 @@ soup = BeautifulSoup(page, "html.parser")
 #    tous les livres d'une categorie
 # ----------------------------------------------------
 
-# 1)  recuperer le nombre de livres dans la categorie
+# 1) recuperer le nombre de livres dans la categorie
 # 2) definir une liste de tous les liens de livres de cette categorie
 # 3) extraire toutes les infos de chaque livre
 
@@ -43,14 +41,40 @@ print("nombre total de livres", nombrelivres)
 
 # 2) definir une liste de tous les liens des livres de cette categorie
 baliseh3 = soup.findAll('h3')
-#print(baliseh3)
+print("balise h3 : ", baliseh3)
 print("nombre de livres dans la page", len(baliseh3))
 
 nbpage = ceil(nombrelivres/20)
 print("nombre de pages de la categorie:", nbpage)
 
-# on definit une liste vide des liens de tous les livres de la categorie
 
+# category : on reupere le nom de la catégorie au niveau du lien de navigation pour les pages suplementaires
+links = []
+for link in soup.find_all('a'):
+    links.append(link.get('href'))
+    #print('linsks :',  links)
+
+#print('len links :', len(links))
+print('links :', links)
+
+# on enleve les 3 premiers elements
+print('links[3:]', links[3:])
+
+liens_categories = links[3:]
+# on laisse uniquement les 51 premiers elements de la liste
+liens_categories = liens_categories[:51]
+print('liens_categories :', liens_categories)
+
+
+# on recupere le nom de la categorie dans l'url reçue
+nom_categorie = url[52:]
+print('nom_categorie -52:', nom_categorie)
+
+# on supprime les derniers caracteres
+nom_categorie = nom_categorie[:-11]
+print('nom_categorie -4:', nom_categorie)
+
+# on definit une liste vide des liens de tous les livres de la categorie
 lienslivres = []
 for x in range(nbpage):
     #print('nbpages', nbpage)
@@ -61,16 +85,16 @@ for x in range(nbpage):
             a = h3.find('a')
             # on recherche l'attribut href contenant le lien url
             lien = a['href']
-            #print(lien[9:])
+            print('lien [9]', lien[9:])
 
             # on rajoute ce lien generique au lien du livre auquel on a enlevé les 9 premiers caractères
             lienslivres.append('https://books.toscrape.com/catalogue/' + lien[9:])
-            print('url 1ère page', url)
-            print(lienslivres)
+            # print('url 1ère page', url)
+            # print(lienslivres)
 
     else:
         # on recupere les liens des pages suivantes
-        urlpsuiv = "https://books.toscrape.com/catalogue/category/books/sequential-art_5/page-" + str(x+1) + ".html"
+        urlpsuiv = "https://books.toscrape.com/catalogue/category/books/" + nom_categorie + "/" + "page-" + str(x+1) + ".html"
         print('url page suivante', urlpsuiv)
 
         reponsesuiv = requests.get(urlpsuiv)
@@ -81,135 +105,18 @@ for x in range(nbpage):
         # on recherche la balise 'a' dans l'objet soup baliseh3
         for h3suiv in baliseh3suiv:
             asuiv = h3suiv.find('a')
-            print('h3 a page suivante', asuiv)
+            #print('h3 a page suivante', asuiv)
 
             # on recherche l'attribut href contenant le lien url
             liensuiv = asuiv['href']
-            print('lien href page suivante', liensuiv)
+            #print('lien href page suivante', liensuiv)
             lienslivres.append('https://books.toscrape.com/catalogue/' + liensuiv[9:])
-            print('url page suivante', urlpsuiv)
+            #print('url page suivante', urlpsuiv)
 
-            print(lienslivres)
+print("liste des liens des livres", lienslivres)
+print("len(lienslivres)", len(lienslivres))
 
-
-# sauvegarder les images
-# sauvegarder
-"""
-# product_page_url
-product_page_url = url
-# affiche dans une liste tous les elements délimités par la balise <td>: voir le contenu de soup
-description = soup.findAll('td')
-#print(description)
-
-# universal_ product_code (upc)
-universal_product_code = description[0].text
-print(universal_product_code)
-
-# title : titre du livre
-title = soup.select("div.product_main>h1")[0].text
-print(title)
-
-# price_including_tax
-price_including_tax = description[2].text
-print(price_including_tax)
-
-# price_excluding_tax
-price_excluding_tax = description[3].text
-print(price_excluding_tax)
-
-# number_available
-number_available = description[5].text
-test = number_available.split("(")[1].split()[0]
-print(test)
-
-# product_description
-product_description = soup.findAll("p")[3].text
-print(product_description)
-
-# category : nom de la catégorie au niveau du lien de navigation
-category = soup.select("ul.breadcrumb>li>a")[2].text
-print(category)
-
-# review_rating
-# review_rating = soup.select("p.star-rating")[0].get("class")[-1]
-review_rating = soup.select("p.star-rating")[0].get("class")[1]
-# print(review_rating)
-
-# conversion de star_rating écrit en lettres en chiffres, ex two = 2 etc
-listetoiles = ["One", "Two", "Three", "For", "Five"]
-# print("len(listetoiles)", len(listetoiles))
-
-for i in range(len(listetoiles)):
-    if listetoiles[i] == review_rating:
-       # print("listetoiles[i]",listetoiles[i])
-       review_rating = i+1
-
-#print('review_rating converti', review_rating)
-print(review_rating)
-
-# image_url : url de l'image du livre
-image_url = "http://books.toscrape.com" + soup.img['src'][5:]
-print(image_url)
-
-
-# création du fichier data.csv pour enregistrer toutes les données extraites d'un livre
-listecolones = ["Code", "Title", "url", "price_including_tax", "price_excluding_tax", "number_available",
-                "product_description", "category", "review_rating", "image_url"]
-
-
-infos_livre = {"Code": universal_product_code, "Title": title, "url": url, "price_including_tax": price_including_tax,
-               "price_excluding_tax": price_excluding_tax, "number_available": number_available,
-               "product_description": product_description, "category": category, "review_rating": review_rating,
-               "image_url": image_url}
-print(infos_livre)
-
-#return infos_livre
-
-with open('unlivreunecat.csv', 'w') as csvfile:
-    writer = csv.DictWriter(csvfile, fieldnames=listecolones)
-    writer.writeheader()
-    # La méthode writerow() est utilisée pour écrire des lignes de données dans le fichier spécifié.
-    writer.writerow(infos_livre)
-    
-"""""
-"""""  
-field_names = ['No', 'Company', 'Car Model'] 
-  
-cars = [ 
-{'No': 1, 'Company': 'Ferrari', 'Car Model': '488 GTB'}, 
-{'No': 2, 'Company': 'Porsche', 'Car Model': '918 Spyder'}, 
-{'No': 3, 'Company': 'Bugatti', 'Car Model': 'La Voiture Noire'}, 
-{'No': 4, 'Company': 'Rolls Royce', 'Car Model': 'Phantom'}, 
-{'No': 5, 'Company': 'BMW', 'Car Model': 'BMW X7'}, ] 
-  
-with open('Names.csv', 'w') as csvfile: 
-    writer = csv.DictWriter(csvfile, fieldnames = field_names) 
-    writer.writeheader() 
-    writer.writerows(cars) 
-    
----------
-def save_book_to_csv(liste: list, name: str) -> None:
-   
-    print(name)
-    fichier = name +".csv" 
-    for livre in liste:  
-        with open(fichier, 'a', newline='', encoding='UTF8') as file:
-            writer = csv.DictWriter(file, fieldnames=list(livre.keys()))
-            if os.stat(fichier).st_size == 0:
-                        # The header is written only if the file is empty
-                        writer.writeheader()
-            writer.writerow(livre)
------------------------
-
-
-# création du fichier data.csv pour enregistrer toutes les données extraite d'un livre
-	en_tete = ['titre', 'description']
-	with open('data.csv', 'w') as fichier_csv:
-		writer = csv.writer(fichier_csv, delimiter=',')
-		writer.writerow(en_tete)
-		# zip permet d'itérer sur deux listes à la fois
-		for titre, description in zip(titre_textes, description_textes):
-			writer.writerow([titre, description])
-
-"""
-
+url = lienslivres[0]
+for url in range(len(lienslivres)):
+    url_livre = lienslivres[url]
+    extract_book(url_livre)
--- unecategorieunlivre.py
+++ unecategorieunlivre.py
@@ -1,154 +1,167 @@
-
 # ----------------------------------------------------
 #    importation des packages necessaires au projet
 # ----------------------------------------------------
 import csv
 import requests
 from bs4 import BeautifulSoup
+import urllib.request
+import os
+import shutil
+
+from pprint import pprint
 
 # on initialise url à l'adresse url de la page qu'on veut extraire
-url = 'https://books.toscrape.com/catalogue/its-only-the-himalayas_981/index.html'
-
-# ..... A DEFINIR APRES fonction qui permet d'extraire toutes les données d'un livre
-# def extract_book(url_book):
-
-# méthode .get()pour récupérer les données HTML dans la variable reponse
-reponse = requests.get(url)
-#print(reponse)
-
-page = reponse.content #text sous forme de texte
-#print(page)
-
-# on parse le code html obtenu avec la package BeautifulSoup
-# A partir de l'objet soup, on obtient des éléments par leur balise, ID ou classe.
-# on obtient une liste de tous les elements
-
-soup = BeautifulSoup(page, "html.parser")
-# print(soup)
+# cat Travel
+# urlbook = 'https://books.toscrape.com/catalogue/its-only-the-himalayas_981/index.html'
 
 # ----------------------------------------------------
-#    RECHERCHE DES INFOS DEMANDEES
+# fonction d'extraction d'informations pour un livre
 # ----------------------------------------------------
+def extract_book(urlbook : str) :
+    reponse = requests.get(urlbook)
+    # print(reponse)
 
-"""
-product_page_url
-universal_ product_code (upc)
-title
-price_including_tax
-price_excluding_tax
-number_available
-product_description
-category
-review_rating
-image_url
-"""
+    page = reponse.content
+    # print(page)
 
+    # on parse le code html obtenu avec la package BeautifulSoup
+    # A partir de l'objet soup, on obtient des éléments par leurs balises, ID ou classes.
+    # on obtient l'objet soup de type liste contenant tous les elements
 
-# product_page_url
-product_page_url = url
-# affiche dans une liste tous les elements délimités par la balise <td>: voir le contenu de soup
-description = soup.findAll('td')
-#print(description)
+    soup = BeautifulSoup(page, "html.parser")
+    # print(soup)
 
-# universal_ product_code (upc)
-universal_product_code = description[0].text
-print(universal_product_code)
+    # ----------------------------------------------------
+    #    RECHERCHE DES INFOS DEMANDEES
+    # ----------------------------------------------------
 
-# title : titre du livre
-title = soup.select("div.product_main>h1")[0].text
-print(title)
+    # product_page_url
+    product_page_url = urlbook
 
-# price_including_tax
-price_including_tax = description[2].text
-print(price_including_tax)
+    # affiche dans une liste tous les elements délimités par la balise <td>: voir le contenu de soup
+    description = soup.findAll('td')
+    #print(description)
 
-# price_excluding_tax
-price_excluding_tax = description[3].text
-print(price_excluding_tax)
+    # universal_ product_code (upc)
+    universal_product_code = description[0].text
+    #print(universal_product_code)
 
-# number_available
-number_available = description[5].text
-test = number_available.split("(")[1].split()[0]
-print(test)
+    # title : titre du livre
+    title = soup.select("div.product_main>h1")[0].text
+    #print(title)
 
-# product_description
-product_description = soup.findAll("p")[3].text
-print(product_description)
+    # price_including_tax
+    price_including_tax = description[2].text
+    #print(price_including_tax)
 
-# category : nom de la catégorie au niveau du lien de navigation
-category = soup.select("ul.breadcrumb>li>a")[2].text
-print(category)
+    # price_excluding_tax
+    price_excluding_tax = description[3].text
+    #print(price_excluding_tax)
 
-# review_rating
-# review_rating = soup.select("p.star-rating")[0].get("class")[-1]
-review_rating = soup.select("p.star-rating")[0].get("class")[1]
-# print(review_rating)
+    # number_available
+    number_available = description[5].text
+    number_available = number_available.split("(")[1].split()[0]
+    #print(test)
 
-# conversion de star_rating écrit en lettres en chiffres, ex two = 2 etc
-listetoiles = ["One", "Two", "Three", "For", "Five"]
-# print("len(listetoiles)", len(listetoiles))
+    # product_description
+    product_description = soup.findAll("p")[3].text
+    #print(product_description)
 
-for i in range(len(listetoiles)):
-    if listetoiles[i] == review_rating:
-       # print("listetoiles[i]",listetoiles[i])
-       review_rating = i+1
+    # category : nom de la catégorie au niveau du lien de navigation
+    category = soup.select("ul.breadcrumb>li>a")[2].text
+    #print(category)
 
-#print('review_rating converti', review_rating)
-print(review_rating)
+    # review_rating
+    # review_rating = soup.select("p.star-rating")[0].get("class")[-1]
+    review_rating = soup.select("p.star-rating")[0].get("class")[1]
+    # print(review_rating)
 
-# image_url : url de l'image du livre
-image_url = "http://books.toscrape.com" + soup.img['src'][5:]
-print(image_url)
+    # conversion de star_rating écrit en lettres en chiffres, ex two = 2 etc
+    listetoiles = ["One", "Two", "Three", "Four", "Five"]
+    # print("len(listetoiles)", len(listetoiles))
 
+    for i in range(len(listetoiles)):
+        if listetoiles[i] == review_rating:
+            # print("listetoiles[i]",listetoiles[i])
+            review_rating = i + 1
 
-# création du fichier data.csv pour enregistrer toutes les données extraites d'un livre
-listecolones = ["Code", "Title", "url", "price_including_tax", "price_excluding_tax", "number_available",
-                "product_description", "category", "review_rating", "image_url"]
+    # print('review_rating converti', review_rating)
+    # print(review_rating)
 
-infos_livre = {"Code": universal_product_code, "Title": title, "url": url, "price_including_tax": price_including_tax,
-               "price_excluding_tax": price_excluding_tax, "number_available": number_available,
-               "product_description": product_description, "category": category, "review_rating": review_rating,
-               "image_url": image_url}
-print(infos_livre)
+    # image_url : url de l'image du livre
 
-#return infos_livre
+    # image_livre = soup.ima['src']
+    #image_livre = soup.findAll("img.src")
+    #print("image_livre", image_livre)
 
-with open('unlivreunecat.csv', 'w') as csvfile:
-    writer = csv.DictWriter(csvfile, fieldnames=listecolones)
-    writer.writeheader()
-    # La méthode writerow() est utilisée pour écrire des lignes de données dans le fichier spécifié.
-    writer.writerow(infos_livre)
+    image_url = "http://books.toscrape.com" + soup.img['src'][5:]
+    # print("image_url :", image_url)
+    # init de info_livress
+    infos_livre = {}
 
-"""        
-------------------------------
-import csv 
-  
-field_names = ['No', 'Company', 'Car Model'] 
-  
-cars = [ 
-{'No': 1, 'Company': 'Ferrari', 'Car Model': '488 GTB'}, 
-{'No': 2, 'Company': 'Porsche', 'Car Model': '918 Spyder'}, 
-{'No': 3, 'Company': 'Bugatti', 'Car Model': 'La Voiture Noire'}, 
-{'No': 4, 'Company': 'Rolls Royce', 'Car Model': 'Phantom'}, 
-{'No': 5, 'Company': 'BMW', 'Car Model': 'BMW X7'}, ] 
-  
-with open('Names.csv', 'w') as csvfile: 
-    writer = csv.DictWriter(csvfile, fieldnames = field_names) 
-    writer.writeheader() 
-    writer.writerows(cars) 
-    
-    
----------
-def save_book_to_csv(liste: list, name: str) -> None:
-    print(name)
-    fichier = name +".csv" 
-    for livre in liste:  
-        with open(fichier, 'a', newline='', encoding='UTF8') as file:
-            writer = csv.DictWriter(file, fieldnames=list(livre.keys()))
-            if os.stat(fichier).st_size == 0:
-                        # The header is written only if the file is empty
-                        writer.writeheader()
-            writer.writerow(livre)
-            
------------------------
-"""
+    # création du fichier data.csv pour enregistrer toutes les données extraites d'un livre
+
+    infos_livre = {"Code": universal_product_code, "Title": title, "url": urlbook, "price_including_tax": price_including_tax,
+                   "price_excluding_tax": price_excluding_tax, "number_available": number_available,
+                   "product_description": product_description, "category": category, "review_rating": review_rating,
+                   "image_url": image_url}
+    # print(infos_livre)
+    #return infos_livre
+
+      # appel de la fonction
+    #listecolones = extract_book(urlbook)
+    print("Dictionnaire:", infos_livre)
+
+    # sauvegarde de l'image à partir du site
+    # on recupere le nom de l'image (qui prend le nom du livre)
+    # on recupre le nom de la categorie pour créer un dossier qui contiendra les images
+
+    # on recupere l'url de l'image
+    urlimage = infos_livre["image_url"]
+    # print("url de l'image: ", urlimage)
+
+    # nom de l'image = nom du livre
+    nom_livre = infos_livre["Title"]
+    # print("nom du livre : ", nom_livre)
+
+    # on enleve les caracteres speciaux des titres ? : ! pour generer un nom d'image correct
+    new_string = ''.join(filter(str.isalnum, nom_livre))
+    # print('titre du livre sans les carac speciaux ! ', new_string)
+    nom_livre = new_string
+
+    titre_image = nom_livre + '_' + urlimage.split("/")[-1]
+    # print("titre de l'image split:", titre_image)
+
+    fichier_image_recupere = urllib.request.urlretrieve(urlimage, titre_image)
+    #print("print IMAGE: ", IMAGE)
+
+    # création d'un dossier qui aura le nom de la categorie
+    nom_dossier = infos_livre["category"] + "_images"
+
+    if not os.path.exists(nom_dossier):
+        os.mkdir(nom_dossier)
+
+        """
+        path = os.path.abspath(nom_dossier)
+        filedest = os.path.join(path, titre_image)
+        # Déplacer un fichier du répertoire rep1 vers rep2
+        shutil.move(titre_image, filedest)
+        """
+    if os.path.exists(nom_dossier):
+        path = os.path.abspath(nom_dossier)
+        filedest = os.path.join(path, titre_image)
+        # Déplacer un fichier du répertoire rep1 vers rep2
+        shutil.move(titre_image, filedest)
+
+    nom_fichier = category + ".csv"
+
+    with open(nom_fichier, 'a') as csvfile :
+        writer = csv.DictWriter(csvfile, fieldnames=infos_livre)
+
+        if os.stat(nom_fichier).st_size == 0:
+            # The header is written only if the file is empty
+            writer.writeheader()
+        # La méthode writerow() est utilisée pour écrire des lignes de données dans le fichier spécifié.
+        writer.writerow(infos_livre)
+
+#extract_book(urlbook)
